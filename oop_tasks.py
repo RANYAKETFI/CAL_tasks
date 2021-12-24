640aed6d721d6ef0ef4ff7393f894533314c0dc5
RANYAKETFI <47351011+RANYAKETFI@users.noreply.github.com>
2021-12-24 18:01:46 +0100

    Update oop_tasks.py

--- oop_tasks.py
+++ oop_tasks.py
@@ -4,19 +4,23 @@ class Rectangle :
     def area():
      return self.width*self.length     
     def __init__(self,len,wid) :
-        self.length=len
-        self.width=wid
+        self.length = len
+        self.width = wid
 
+ 
  
 #Create a Vehicle class with max_speed and mileage instance attributes
 class Vehicle:
     def __init__(self,max,mile):
-        self.max_speed=max
-        self.mileage=mile
+        self.max_speed = max
+        self.mileage = mile
+
+
 #Create a Vehicle class without any variables and methods.
 class Vehicle:
     pass
         
+
 #Create a child class Bus that will inherit all of the variables and methods of the Vehicle class 
-class Child(Vehicle):
+class Bus(Vehicle):
    pass
